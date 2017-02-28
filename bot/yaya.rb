9980771ff28b33bf214ea64e4a82bc9a8186cf4f
anoChick <anoinchick@gmail.com>
2017-02-28 20:53:48 +0900

    appnameを返すコマンド

--- bot/yaya.rb
+++ bot/yaya.rb
@@ -1,5 +1,5 @@
 class Yaya < SlackRubyBot::Bot
   command 'say' do |client, data, match|
-    send_message client, data.channel, match['expression']
+    send_message client, data.channel, ENV['HEROKU_APP_NAME']
   end
 end
