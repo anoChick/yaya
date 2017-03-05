b21caaa0d0038f99b1367654709495bef7679ff4
Dueno <anoinchick@gmail.com>
2017-03-06 02:36:41 +0900

    bugfix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: (ENV['REDIS_URL']).to_s, namespace: 'sidekiq' }
+  config.redis = { url: 'redis://localhost:6379'}
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: (ENV['REDIS_URL']).to_s, namespace: 'sidekiq' }
+  config.redis = { url: 'redis://localhost:6379'}
 end
