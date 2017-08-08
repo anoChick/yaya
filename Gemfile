c656d667c2177a4119a5acd0d1e45028d2ebb954
anoChick <anoinchick@gmail.com>
2017-08-08 23:51:05 +0900

    Add Sidekiq

--- Gemfile
+++ Gemfile
@@ -30,3 +30,4 @@ group :development do
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'sidekiq'
--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Yaya
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.active_job.queue_adapter = :sidekiq
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
