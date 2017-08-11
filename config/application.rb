fd9f7ae4397ef442c37c12c12dfc78db86149b8e
anoChick <anoinchick@gmail.com>
2017-08-11 12:43:24 +0900

    Added autoload

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,9 @@ module Yaya
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.active_job.queue_adapter = :sidekiq
-
+    config.autoload_paths << Rails.root.join('bot')
+    config.autoload_paths << Rails.root.join('app','services')
+    config.autoload_paths << Rails.root.join('app','jobs')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
