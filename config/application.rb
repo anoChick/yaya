332761c14e2be487a3c56a7405cc7c7a80af17a0
anoChick <anoinchick@gmail.com>
2017-08-11 16:37:34 +0900

    Set tiemzone

--- config/application.rb
+++ config/application.rb
@@ -26,13 +26,7 @@ module Yaya
     config.autoload_paths << Rails.root.join('bot')
     config.autoload_paths << Rails.root.join('app','services')
     config.autoload_paths << Rails.root.join('app','jobs')
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
+    config.time_zone = 'Tokyo'
     config.api_only = true
   end
 end
