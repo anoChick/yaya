b62454f8cf3ecd1700bf679990622a53171ae685
anoChick <anoinchick@gmail.com>
2017-08-20 02:12:15 +0900

    Refactoring base class post_message

--- bot/commands/code_register.rb
+++ bot/commands/code_register.rb
@@ -7,18 +7,17 @@ module YayaBot
         spell_name = match[:expression]
 
         code = current_character.codes.find_by(name: spell_name)
-        if code.try(:user).present?
-          next client.web_client.chat_postMessage(
-            channel: data.channel,
-            text: "#{name}は既に#{code.user.name}によって記録されています。",
-            username: current_character.name,
-            icon_url: current_character.icon_url
-          )
-        end
+        return post_message("#{spell_name}は既に#{code.user.name}によって記録されています。") if code.try(:user).present?
         Code.waiting_spell(spell_name, current_user, current_character)
-        post_message(
-          "@#{current_user.name} 新しいコマンド、[#{spell_name}]を記録。コードを記述しSnippetとして[Command+Enter]投稿してください。"
-        )
+        post_message("@#{current_user.name} 新しいコマンド、[#{spell_name}]を記録。コードを記述しSnippetとして投稿してください。")
+      end
+
+      character_command '忘れる' do |client, data, match|
+        code = current_character.codes.find_by(name: match[:expression])
+        return post_message("[#{code.name}]は登録されていません。") if code.nil?
+        return post_message("[#{code.name}]はあなたに教わった魔術ではありません。") if code&.user&.id != current_user.id
+        code.destroy
+        post_message("[#{code.name}]はきれいさっぱり忘れました。")
       end
     end
   end
--- bot/commands/custom_command.rb
+++ bot/commands/custom_command.rb
@@ -4,7 +4,11 @@ module YayaBot
   module Commands
     class CustomCommand < Base
       custom_command do |client, data, match|
-        binding.pry
+        command_name = match['command']
+        return post_message("[#{command_name}は記録されていません。]")if current_character.codes.find_by(name: command_name).nil?
+
+        ExecCommandJob.perform(data.to_json)
+        #ExecCommandJob.perform_later(data.to_json)
       end
     end
   end
